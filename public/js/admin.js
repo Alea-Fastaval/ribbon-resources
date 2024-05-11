c78be779b3738e996103a639747d90af3c0f7426
DreamSpawn <westh_mikkel@hotmail.com>
2024-05-11 20:24:51 +0200

    ADDED admin page now renders category header and button for adding category ADDED admin page uses translations from server

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,15 +1,53 @@
 "use strict";
 
 $(function() {
-  let categories
+
+  let need_loading = {
+    categories: true,
+    page_translations: true,
+  }
+
+  function resource_loaded(key) {
+    delete need_loading[key];
+    if (Object.keys(need_loading).length == 0) {
+      Admin.render_page()
+    }
+  }
   
   $.ajax({
     url: "/api/categories",
-    succes: function(data, status) {
-      categories = JSON.parse(data);
+    success: function(data, status) {
+      Admin.categories = data;
+      resource_loaded("categories");
     }
   })
 
-  let content = $(".main-container");
-  content.append(Render.foldingSection("Kategorier"));
+  $.ajax({
+    url: `/api/translations/${page_info.lang}/admin/`,
+    success: function(data, status) {
+      Admin.translations.page = data;
+      resource_loaded("page_translations")
+    }
+  })
+
 });
+
+class Admin {
+  static categories
+  static translations = {}
+
+  static render_page() {
+    let pt = this.translations.page;
+
+    let main_element = $(".main-container");
+
+    let categories_content = $("<div></div>")
+
+    let categories_element = Render.foldingSection(pt.categories, categories_content);
+    main_element.append(categories_element);
+
+    let new_category_button = $(`<div class="dummy-category category" style="background-color: var(--highlight-color)">${pt.new_category}</div>`)
+    categories_content.append(new_category_button)
+
+  }
+}
