7b2912a1eb3a102fcdb3cc3022c95a591dd044fa
DreamSpawn <westh_mikkel@hotmail.com>
2024-10-06 03:06:43 +0200

    ADDED ordering to ribbons table

--- database/1/ribbons.sql
+++ database/1/ribbons.sql
@@ -3,6 +3,7 @@ CREATE TABLE ribbons (
   category_id INT UNSIGNED NOT NULL,
   glyph_id INT UNSIGNED NOT NULL,
   no_wings BOOLEAN NOT NULL DEFAULT FALSE,
+  ordering int unsigned DEFAULT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (category_id) REFERENCES categories(id),
   FOREIGN KEY (glyph_id) REFERENCES glyphs(id)
