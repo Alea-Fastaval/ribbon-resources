51824d63b4d6386efc62a180dc8bf141f6a35774
DreamSpawn <westh_mikkel@hotmail.com>
2025-03-19 02:55:38 +0100

    ADDED user.css for styling the user page ADDED year selection and save button to ribbon-list

--- public/js/user.js
+++ public/js/user.js
@@ -34,6 +34,8 @@ class UserPage {
 
   static render_page() {
     Render.init(Ribbon.translations);
+    let pt = Ribbon.translations.page;
+    let gt = Ribbon.translations.general;
 
     UserPage.selection_section = $('#selection-section');
     UserPage.selection_section.append(`<h3>${Ribbon.translations.page.selection_header}:</h3>`);
@@ -48,15 +50,80 @@ class UserPage {
       UserPage.selection_section.append(category_element);  
 
       let ribbon_list = category_element.find(".folding-section-content");
+      ribbon_list.addClass('ribbon-list');
+
+
+      let header_texts = [
+        'grunt',
+        'second',
+        'leader',
+        'total',
+      ];
+
+      let column = 2
+      for (const text of header_texts) {
+        let header_element = $(`<div class="ribbon-list-header">${pt[text]}</div>`)
+        header_element.css('grid-column', column++)
+        ribbon_list.append(header_element)
+      }
 
       //-------------------------------------------
       // Render Ribbons
       //-------------------------------------------
       if (Ribbon.ribbons[category.ID]) for (const ribbon of Ribbon.ribbons[category.ID]) {
+        // Ribbon info
         ribbon.name = Ribbon.translations.ribbons[ribbon.ID].name;
         ribbon.desc = Ribbon.translations.ribbons[ribbon.ID].desc;
         let ribbon_element = Render.ribbon(ribbon, category);
+        ribbon_element.css('grid-column', 1);
         ribbon_list.append(ribbon_element);
+
+        // Number dials
+        let grunt = Render.number_dial();
+        grunt.css('grid-column', 2);
+        grunt.attr('ribbon-id', ribbon.ID);
+        ribbon_list.append(grunt);
+
+        if (ribbon.NoWings === false) {
+          let second = Render.number_dial();
+          second.css('grid-column', 3);
+          second.attr('ribbon-id', ribbon.ID);
+          ribbon_list.append(second);
+
+          let leader = Render.number_dial();
+          leader.css('grid-column', 4);
+          leader.attr('ribbon-id', ribbon.ID);
+          ribbon_list.append(leader);
+        }
+
+        // Total years
+        let total_element = $(`<div class="ribbon-list-total"></div>`);
+        total_element.css('grid-column', 5);
+        total_element.attr('ribbon-id', ribbon.ID);
+        ribbon_list.append(total_element);
+
+        // Preview
+        
+        // Save button
+        let save_button = $(`<button class="ribbon-list-save-button">${gt.save}</button>`);
+        save_button.css('grid-column', 7);
+        save_button.attr('ribbon-id', ribbon.ID);
+        save_button.prop('disabled', true);
+        ribbon_list.append(save_button);
+        
+        // Calculate total
+        let inputs = $(`.number-dial-wrapper[ribbon-id=${ribbon.ID}] input`);
+        inputs.on('change', () => {
+          let total = 0;
+          let change = false;
+          inputs.each((index, element) => {
+            if ($(element).val() != $(element).attr('initial-value')) change = true;
+            let value = parseInt($(element).val());
+            total += isNaN(value) ? 0 : value;
+          });
+          total_element.html(total == 0 ? "" : total + " " + pt.years);
+          save_button.prop('disabled', !change);
+        });
       }
     }
   }
