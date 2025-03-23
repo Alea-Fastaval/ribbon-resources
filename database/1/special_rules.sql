a339bc4dbad43481a0da720cbd814355f05fd12b
DreamSpawn <westh_mikkel@hotmail.com>
2025-03-23 21:25:34 +0100

    ADDED special_rules table definition and rule example for the silver dragon

---
+++ database/1/special_rules.sql
@@ -0,0 +1,10 @@
+CREATE TABLE special_rules (
+  id INT UNSIGNED AUTO_INCREMENT,
+  ribbon_id INT UNSIGNED,
+  name TEXT NOT NULL,
+  value TEXT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (ribbon_id) REFERENCES ribbons(id)
+) ENGINE=InnoDB CHARSET=utf8mb4;
+
+INSERT INTO special_rules (ribbon_id, name, value) VALUES (,"glyph_color","#cccccc"); -- For the silver dragon
